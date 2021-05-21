62bcc59502dbf435ef8951cd82c6039053df6356
takuto121 <47590406+takuto121@users.noreply.github.com>
2021-05-21 15:39:37 +0900

    Change OpenCV version 4.5.1 to 3.4.1

--- scripts/bash/install-opencv.sh
+++ scripts/bash/install-opencv.sh
@@ -5,7 +5,7 @@
 # -------------------------------------------------------------------- |
 #                       SCRIPT OPTIONS                                 |
 # ---------------------------------------------------------------------|
-OPENCV_VERSION='4.5.1'       # Version to be installed
+OPENCV_VERSION='3.4.1'       # Version to be installed
 OPENCV_CONTRIB='NO'          # Install OpenCV's extra modules (YES/NO)
 # -------------------------------------------------------------------- |
 
